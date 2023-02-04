a3bf382ac54437c76e60a6185abbd812ce898a4c
Fidelis <fideliofidel9@gmail.com>
2023-02-04 20:55:39 +0300

    Created class component prop in AppJs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,29 @@
+import React from 'react';
 import './App.css';
+
+import PlayList from '../PlayList/PlayList';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Spotify from '../util/Spotify';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      SearchResults: [],
+      PlayListName: "New playlist",
+      PlayListTracks: []
+    };
+
+    this.search = this.search.bind(this);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlayListName = this.updatePlayListName.bind(this);
+    this.savePlayList = this.savePlayList.bind(this);
+    this.removeTrackSearch = this.removeTrackSearch(this);
+  }
+}
 
 function App() {
   return (
